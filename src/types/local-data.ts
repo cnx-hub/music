8203ff3a8e1e999f2161335ba3e19007f6bb83b3
chennanxiaogit1 <1821172805@qq.com>
2022-02-23 23:38:32 +0800

    feat: 添加本地数据

--- src/types/local-data.ts
+++ src/types/local-data.ts
@@ -16,3 +16,24 @@ export interface IDicoverMenu {
   link: string
   title: string
 }
+
+export interface IHotRadios {
+  picUrl: string
+  name: string
+  position: string
+  url: string
+}
+
+interface IArtists {
+  name: string
+  type: number
+  url: string
+  id?: number
+  dataPath?: string
+}
+
+export interface IArtistCategories {
+  title: string
+  area: number
+  artists: IArtists[]
+}
