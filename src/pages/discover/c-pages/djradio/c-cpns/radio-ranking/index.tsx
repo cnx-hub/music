918224158c408104e984200214e4265928a23bb8
chennanxiaogit1 <1821172805@qq.com>
2022-02-27 17:22:16 +0800

    feat: 电台排行

---
+++ src/pages/discover/c-pages/djradio/c-cpns/radio-ranking/index.tsx
@@ -0,0 +1,5 @@
+import React, { memo } from 'react'
+
+export default memo(function HYRadioRanking() {
+  return <div>index</div>
+})
--- src/pages/discover/c-pages/djradio/index.tsx
+++ src/pages/discover/c-pages/djradio/index.tsx
@@ -2,11 +2,13 @@ import React, { memo } from 'react'
 
 import { DjRadioWrapper } from './style'
 import HYRadioCategory from './c-cpns/radio-category'
+import HYRadioRanking from './c-cpns/radio-ranking'
 
 export default memo(function HYDjradio() {
   return (
     <DjRadioWrapper className="wrap-v2">
       <HYRadioCategory />
+      <HYRadioRanking />
     </DjRadioWrapper>
   )
 })
