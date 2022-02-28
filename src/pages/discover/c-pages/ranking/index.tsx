8e546dffc129f54fdebe9278fb1fcb92e152c4d2
chennanxiaogit1 <1821172805@qq.com>
2022-02-28 17:30:01 +0800

    feat: 排行布局

--- src/pages/discover/c-pages/ranking/index.tsx
+++ src/pages/discover/c-pages/ranking/index.tsx
@@ -1,5 +1,12 @@
 import React, { memo } from 'react'
 
+import { RankingLeft, RankingRight, RankingWrapper } from './style'
+
 export default memo(function HYRanking() {
-  return <div>ranking</div>
+  return (
+    <RankingWrapper className="wrap-v2">
+      <RankingLeft>LEFT</RankingLeft>
+      <RankingRight>RIGHT</RankingRight>
+    </RankingWrapper>
+  )
 })
