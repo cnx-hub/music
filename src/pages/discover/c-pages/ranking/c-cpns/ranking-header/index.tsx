e92eb46e7b7dcb42979d7104be016461213f05de
chennanxiaogit1 <1821172805@qq.com>
2022-02-28 23:58:41 +0800

    feat: 排行榜的头部

--- src/pages/discover/c-pages/ranking/c-cpns/ranking-header/index.tsx
+++ src/pages/discover/c-pages/ranking/c-cpns/ranking-header/index.tsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, { memo } from 'react'
+import { useSelector, shallowEqual } from 'react-redux'
+import { fromJS } from 'immutable'
 
-export default function HYRankingHeader() {
-  return <div>index</div>
-}
+import { formatMonthDay } from 'utils/format-utils'
+
+import { RankingHeaderWrapper } from './style'
+import HYSongOperationBar from 'components/song-operation-bar'
+
+import type { rootState } from 'store'
+
+export default memo(function HYRankingHeader() {
+  // redux
+  const { playList } = useSelector<rootState, { playList: any }>(
+    (state) => ({
+      playList: fromJS(state).getIn(['ranking', 'playList']) as any
+    }),
+    shallowEqual
+  )
+
+  return (
+    <RankingHeaderWrapper>
+      <div className="image">
+        <img src={playList.coverImgUrl} alt="" />
+        <span className="image_cover">封面</span>
+      </div>
+      <div className="info">
+        <div className="title">{playList.name}</div>
+        <div className="time">
+          <i className="clock sprite_icon2"></i>
+          <div>最近更新：{formatMonthDay(playList.updateTime)}</div>
+          <div className="update-f">（{'每日更新:TODO'}）</div>
+        </div>
+        <HYSongOperationBar
+          favorTitle={`(${playList.subscribedCount})`}
+          downloadTitle="下载"
+          commentTitle={`(${playList.commentCount})`}
+          shareTitle={`(${playList.shareCount})`}
+        />
+      </div>
+    </RankingHeaderWrapper>
+  )
+})
--- src/utils/format-utils.ts
+++ src/utils/format-utils.ts
@@ -13,3 +13,43 @@ export function getCount(count: number) {
     return Math.floor(count / 10000000) / 10 + '亿'
   }
 }
+
+export function formatDate(time: number, fmt: string) {
+  let date = new Date(time)
+
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(
+      RegExp.$1,
+      (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+    )
+  }
+  let o: any = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  }
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + ''
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? str : padLeftZero(str)
+      )
+    }
+  }
+  return fmt
+}
+
+function padLeftZero(str: string) {
+  return ('00' + str).substr(str.length)
+}
+
+export function formatMonthDay(time: number) {
+  return formatDate(time, 'MM月dd日')
+}
+
+export function formatMinuteSecond(time: number) {
+  return formatDate(time, 'mm:ss')
+}
