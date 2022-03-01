3e2ca0531bf50ab1de99df8e6631eec015521a5b
chennanxiaogit1 <1821172805@qq.com>
2022-03-01 22:36:51 +0800

    feat: firend

--- src/pages/friend/index.tsx
+++ src/pages/friend/index.tsx
@@ -1,5 +1,17 @@
 import React, { memo } from 'react'
 
-export default memo(function Friend() {
-  return <div>Friend</div>
+import { FriendWrapper } from './style'
+
+export default memo(function HYFriend() {
+  return (
+    <FriendWrapper>
+      <div className="content wrap-v2">
+        <div className="pic">
+          <a className="login" href="/#">
+            立即登录
+          </a>
+        </div>
+      </div>
+    </FriendWrapper>
+  )
 })
---
+++ src/pages/friend/style.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const FriendWrapper = styled.div`
+  background: #f5f5f5;
+  .content {
+    background-color: #fff;
+    min-height: 700px;
+
+    .pic {
+      position: relative;
+      width: 807px;
+      height: 484px;
+      margin: 0 auto;
+      background: url(${require('assets/img/friend_sprite.jpg')}) 0 104px
+        no-repeat;
+
+      .login {
+        position: absolute;
+        width: 167px;
+        height: 45px;
+        left: 482px;
+        top: 368px;
+        text-indent: -9999px;
+      }
+    }
+  }
+`
